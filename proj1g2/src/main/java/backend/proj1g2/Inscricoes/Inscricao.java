73e470aca8668dd38ff33cbeb2aeb96abfd983f0
luigilopesz <luigilopes09@gmail.com>
2024-10-29 18:01:20 -0300

    esqueleto da Inscricao

---
+++ proj1g2/src/main/java/backend/proj1g2/Inscricoes/Inscricao.java
@@ -0,0 +1,24 @@
+package backend.proj1g2.Inscricoes;
+
+import jakarta.validation.constraints.NotEmpty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document
+@Data
+public class Inscricao {
+
+    @Id
+    private String id;
+
+    @NotEmpty
+    private String user;
+
+    @NotEmpty
+    private String event;
+
+    public Inscricao(String user, String event) {
+    }
+}
---
+++ proj1g2/src/main/java/backend/proj1g2/Inscricoes/InscricaoDTO.java
@@ -0,0 +1,4 @@
+package backend.proj1g2.Inscricoes;
+
+public record InscricaoDTO() {
+}
