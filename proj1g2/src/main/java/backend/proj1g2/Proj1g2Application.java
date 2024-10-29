f3fd1e02988226619e79bef41cdf3958f9b0cd57
luigilopesz <luigilopes09@gmail.com>
2024-10-29 16:59:26 -0300

    projeto

---
+++ proj1g2/src/main/java/backend/proj1g2/Proj1g2Application.java
@@ -0,0 +1,13 @@
+package backend.proj1g2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Proj1g2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Proj1g2Application.class, args);
+	}
+
+}
---
+++ proj1g2/src/test/java/backend/proj1g2/Proj1g2ApplicationTests.java
@@ -0,0 +1,13 @@
+package backend.proj1g2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Proj1g2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
